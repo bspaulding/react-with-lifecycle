eae3069ae3447252ccddef3053ea804933260a6b
Bradley Spaulding <brad.spaulding@gmail.com>
2016-10-27 13:15:09 -0700

    Fixed module name in rollup config
    also added flow annotation, fixed lint errors

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,15 @@
+/* @flow */
+
 import babel from 'rollup-plugin-babel';
 
 export default {
 	dest: 'dist/bundle.js',
 	entry: 'src/index.js',
 	format: 'umd',
-	moduleName: 'react-with-lifecycle',
+	moduleName: 'reactWithLifecycle',
 	plugins: [babel({
 		babelrc: false,
 		presets: ['es2015-rollup', 'react'],
 		plugins: ['transform-flow-strip-types']
 	})]
-}
+};
